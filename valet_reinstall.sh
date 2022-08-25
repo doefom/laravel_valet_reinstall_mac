afb195449739f3650eda316bd18cbd96dc13a608
doefom <33541715+doefom@users.noreply.github.com>
2022-08-25 21:03:39 +0200

    Update valet_reinstall.sh

--- valet_reinstall.sh
+++ valet_reinstall.sh
@@ -1,12 +1,18 @@
-# Chown homebrew directory for current user
-sudo chown -R $(whoami) /opt/homebrew
+echo "Chown homebrew directory for current user"
+sudo chown -R "$(whoami)" /opt/homebrew
 
-# Uninstall Laravel Valet
+echo "Uninstall Laravel Valet"
 composer global remove laravel/valet
-brew uninstall dnsmasq
-sudo rm -r ~/.config/valet
-rm -r $(which valet)
 
-# Install Laravel Valet
+echo "Uninstall dnsmasq"
+brew uninstall dnsmasq
+
+echo "Remove Laravel Valet config at default location"
+sudo rm -r ~/.config/valet
+
+echo "Remove Laravel Valet composer binary"
+rm -r "$(which valet)"
+
+echo "Install Laravel Valet"
 composer global require laravel/valet
 valet install
